0af5e3537b22548d19609cc8bcb2dfd7af90cb20
Vesta Manuyko <Vesta.manuyko@gmail.com>
2026-04-29 10:12:47 +0200

    explicit message for valgrind

--- tests/check_valid.c
+++ tests/check_valid.c
@@ -5,7 +5,7 @@ void	check_valid(t_result res)
 	if (strncmp(res.stderr, "valgrind", 8) == 0)
 		cr_assert_fail("Executable doesnt exist");
 	if (res.exit_code == 42)
-		cr_assert_fail("Valgrind detected leaks");
+		cr_assert_fail("Valgrind detected errors");
 	else if (res.exit_code != 0 && res.exit_code != 42)
 		cr_assert_fail("Exit code should be 0 for success");
 }
